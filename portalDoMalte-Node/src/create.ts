028c0671dda18760a3095ea568fffd02ef7e2571
JessicaPersou <jessica.sousa@koin.com.br>
2022-11-18 17:08:49 -0300

    create db and tables client,products

---
+++ portalDoMalte-Node/src/create.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main(){
+  const create = await prisma.client.create({
+    data:{
+    name :"Jessica Sousa",
+    cpf : "111.222.333-00",
+    phone : "11989898000",
+    birthdate: "1993-03-19",
+    email: "jessica.persou@gmail.com",
+    password: "teste123"
+    }
+  })
+  console.log(create);
+}
+
+main();
