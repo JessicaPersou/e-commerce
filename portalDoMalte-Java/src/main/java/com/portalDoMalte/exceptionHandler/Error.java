8acbe51e4fb74684d778cf3f683232b1b60432ba
Jessica P Sousa <jessica.persou@gmail.com>
2022-12-29 17:22:06 -0300

    Exception handler to developer and user

---
+++ portalDoMalte-Java/src/main/java/com/portalDoMalte/exceptionHandler/Error.java
@@ -0,0 +1,15 @@
+package com.portalDoMalte.exceptionHandler;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Error {
+
+    private String messageUser;
+    private String messageDeveloper;
+
+}
